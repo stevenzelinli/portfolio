07dc3c3652dd84b2ecc32bb3ded29c6382c32a2f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-28 18:09:32 +0000

    Deploy stevenzelinli/portfolio to github.com/stevenzelinli/portfolio.git:gh-pages

--- README.md
+++
@@ -1,3 +0,0 @@
-## Pipelines
-## master: [![Build Status](https://travis-ci.com/stevenzelinli/portfolio.svg?branch=master)](https://travis-ci.com/stevenzelinli/portfolio)
-## develop: [![Build Status](https://travis-ci.com/stevenzelinli/portfolio.svg?branch=develop)](https://travis-ci.com/stevenzelinli/portfolio)
---
+++ precache-manifest.5ebbf18283e8b15eaf0309d9f61b0bf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "874bcddac322213b5c02c39eb4ffe802",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "71f78a682685b70e1812",
+    "url": "/portfolio/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "687816e9d7af14284897",
+    "url": "/portfolio/static/css/main.28dbfe5e.chunk.css"
+  },
+  {
+    "revision": "71f78a682685b70e1812",
+    "url": "/portfolio/static/js/2.fc60e946.chunk.js"
+  },
+  {
+    "revision": "687816e9d7af14284897",
+    "url": "/portfolio/static/js/main.fa262553.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
